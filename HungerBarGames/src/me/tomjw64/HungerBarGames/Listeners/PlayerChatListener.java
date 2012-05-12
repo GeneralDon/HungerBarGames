8a104b2d4a12f632009010d977845b9a82603e0d
Thomas <Thomas@Cooler_Master>
2012-05-11 22:30:31 -0500

    Implemented Listener.

--- HungerBarGames/src/me/tomjw64/HungerBarGames/Listeners/PlayerChatListener.java
+++ HungerBarGames/src/me/tomjw64/HungerBarGames/Listeners/PlayerChatListener.java
@@ -4,12 +4,11 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.player.PlayerChatEvent;
 
-public class PlayerChatListener {
+public class PlayerChatListener implements Listener{
 	
 	@EventHandler(priority=EventPriority.NORMAL)
-	public void Chat(PlayerChatEvent event) {
-		
-		
+	public void chat(PlayerChatEvent chat)
+	{
 		
 	}
 	
