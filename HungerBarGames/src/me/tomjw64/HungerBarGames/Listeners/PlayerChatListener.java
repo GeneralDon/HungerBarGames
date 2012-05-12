31ac697094580454765f8d9ce1a7b44f5bc0ff11
Thomas <Thomas@Cooler_Master>
2012-05-11 22:30:54 -0500

    Imported Listener.

--- HungerBarGames/src/me/tomjw64/HungerBarGames/Listeners/PlayerChatListener.java
+++ HungerBarGames/src/me/tomjw64/HungerBarGames/Listeners/PlayerChatListener.java
@@ -2,6 +2,7 @@ package me.tomjw64.HungerBarGames.Listeners;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerChatEvent;
 
 public class PlayerChatListener implements Listener{
