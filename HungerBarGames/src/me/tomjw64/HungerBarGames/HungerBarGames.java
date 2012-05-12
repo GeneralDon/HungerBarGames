e9c575d6a5c0acafc023a1bc8b42502c607f0200
Thomas <Thomas@Cooler_Master>
2012-05-11 20:30:53 -0500

    Added plugin info command.

--- HungerBarGames/src/me/tomjw64/HungerBarGames/HungerBarGames.java
+++ HungerBarGames/src/me/tomjw64/HungerBarGames/HungerBarGames.java
@@ -12,6 +12,7 @@ import me.tomjw64.HungerBarGames.Managers.ConfigManager;
 import me.tomjw64.HungerBarGames.Managers.DataManager;
 import me.tomjw64.HungerBarGames.Managers.PlayerDataManager;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.PluginDescriptionFile;
@@ -36,8 +37,6 @@ public class HungerBarGames extends JavaPlugin{
 		ConfigManager.loadConfig(this);
 		PlayerDataManager.loadPlayerData(this);
 		
-		//Load Arenas
-		
 		logger.info("["+pdf.getName()+"] version "+pdf.getVersion()+" is enabled!");
 	}
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args)
@@ -50,7 +49,12 @@ public class HungerBarGames extends JavaPlugin{
 			}
 			else
 			{
-				//Display plugin info
+				ChatColor YELLOW=ChatColor.YELLOW;
+				ChatColor AQUA=ChatColor.AQUA;
+				ChatColor GREEN=ChatColor.GREEN;
+				sender.sendMessage(YELLOW+"HungerBarGames version "+AQUA+getDescription().getVersion()+YELLOW+".");
+				sender.sendMessage(YELLOW+"Made by "+GREEN+"tomjw64"+YELLOW+" and "+GREEN+"HaydenCappldona"+YELLOW+"!");
+				sender.sendMessage("Type "+AQUA+"/hbg help"+YELLOW+" for HungerBarGames commands!");
 			}
 		}
 		return true;
