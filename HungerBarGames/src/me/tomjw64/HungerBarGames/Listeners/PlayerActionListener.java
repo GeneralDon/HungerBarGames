9ca758a3acd3e831f3bb0c2fd4426865b1e1d546
HaydenCappadona <ha_cappadona@hotmail.com>
2012-05-12 12:38:20 +1000

    Added a separate PlayerChatListener Class.

--- HungerBarGames/src/me/tomjw64/HungerBarGames/Listeners/PlayerActionListener.java
+++ HungerBarGames/src/me/tomjw64/HungerBarGames/Listeners/PlayerActionListener.java
@@ -7,7 +7,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
-import org.bukkit.event.player.PlayerChatEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public class PlayerActionListener extends GameListener{
@@ -52,9 +51,5 @@ public class PlayerActionListener extends GameListener{
 				dmg.setCancelled(true);
 			}
 		}
-	}
-	@EventHandler(priority=EventPriority.NORMAL)
-	public void onPlayerChat(PlayerChatEvent event) {
-		
 	}
 }
