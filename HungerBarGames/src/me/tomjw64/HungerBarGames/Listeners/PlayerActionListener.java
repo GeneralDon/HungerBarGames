87e9bbf3aab5be595b78702745a5bfe2e18a1713
Thomas <Thomas@Cooler_Master>
2012-05-13 16:04:08 -0500

    Fixed Listener

--- HungerBarGames/src/me/tomjw64/HungerBarGames/Listeners/PlayerActionListener.java
+++ HungerBarGames/src/me/tomjw64/HungerBarGames/Listeners/PlayerActionListener.java
@@ -25,7 +25,6 @@ public class PlayerActionListener extends GameListener{
 	@EventHandler(priority=EventPriority.NORMAL)
 	public void death(PlayerDeathEvent death)
 	{
-		death.getEntity().getServer().broadcastMessage("Event fired!");
 		if(getGame().isTribute(death.getEntity()))
 		{
 			Player dead=death.getEntity();
